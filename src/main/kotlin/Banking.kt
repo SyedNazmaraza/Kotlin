31c8270036097d05b10beb5c1ac8864b9466b80c
SyedNazmaraza <syed.n@marketsimplified.com>
2023-06-12 14:18:40 +0530

    first

--- src/main/kotlin/Banking.kt
+++ src/main/kotlin/Banking.kt
@@ -7,31 +7,47 @@ fun main() {
 
 }
 sealed class BankingAccount(var limit:Int){
-    abstract fun depositing();
-    abstract fun withdrawing();
+    abstract fun depositing(amount:Int);
+    abstract fun withdrawing(amount:Int);
     class SavingAccount(var accountNumber: Int , var balance: Int): BankingAccount(10000) {
-        override fun depositing() {
+        override fun depositing(amount:Int) {
             println("Doing deposit in saving_account")
         }
-        override fun withdrawing() {
-            println("Doing withdraw in saving_account with limit $limit")
+        override fun withdrawing(amount:Int) {
+            if(amount<= limit){
+                println("Doing withdraw in SavingAccount ")
+            }
+            else{
+                println("your withdraw limit is $limit")
+            }
         }
 
     }
     class CurrentAccount(var accountNumber:Int,var balance:Int):BankingAccount(15000){
-        override fun depositing() {
+        override fun depositing(amount:Int) {
             println("Doing deposit in CurrentAccount")
         }
-        override fun withdrawing() {
-            println("Doing withdraw in CurrentAccount with limit $limit")
+        override fun withdrawing(amount:Int) {
+            if(amount<= limit){
+                println("Doing withdraw in CurrentAccount ")
+            }
+            else{
+                println("your withdraw limit is $limit")
+            }
         }
     }
     class FixedDepositAccount(var accountNumber:Int,var balance:Int):BankingAccount(20000){
-        override fun depositing() {
+        override fun depositing(amount:Int) {
             println("Doing deposit in FixedDepositAccount")
         }
-        override fun withdrawing() {
-            println("Doing withdraw in FixedDepositAccount with limit $limit")
+        override fun withdrawing(amount:Int) {
+            if(amount<= limit){
+                println("Doing withdraw in FixedDepositAccount ")
+            }
+            else{
+                println("your withdraw limit is $limit")
+            }
+
         }
     }
 }
